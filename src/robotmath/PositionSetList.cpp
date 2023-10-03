0b7ed55c50521c239d202c444ea165d224c53885
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-10-03 17:59:09 -0500

    add methods

--- src/robotmath/PositionSetList.cpp
+++ src/robotmath/PositionSetList.cpp
@@ -1,1 +1,47 @@
 #include "Path.h"
+
+void Path::addToStart(positionSet pose) {
+    PathNode* n = new PathNode();
+    n->path = this;
+    n->pose = pose;
+    n->previous = nullptr;
+    n->next = this->start;
+
+    this->start = n;
+    if (size == 0) {
+        this->end = n;
+    }
+
+    size++;
+}
+
+void Path::addToEnd(positionSet pose) {
+    PathNode* n = new PathNode();
+    n->path = this;
+    n->pose = pose;
+    n->previous = this->end;
+    n->next = nullptr;
+
+    this->end = n;
+    if (size == 0) {
+        this->start = n;
+    }
+
+    size++;
+}
+
+void Path::addToStart(Path other) {
+    PathNode* n = other.end;
+    while (n) {
+        this->addToStart(n->pose);
+        n = n->previous;
+    }
+}
+
+void Path::addToEnd(Path other) {
+    PathNode* n = other.start;
+    while (n) {
+        this->addToEnd(n->pose);
+        n = n->next;
+    }
+}
